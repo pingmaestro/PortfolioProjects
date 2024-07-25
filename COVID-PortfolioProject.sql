445d72a30e3961e9b94c97c2f5d5cac9ce8e7ae9
pingmaestro <guillaumelagace514@gmail.com>
2024-07-25 13:07:48 -0400

    Add files via upload
    Quelques corrections sur l'exercice d'hier

--- COVID-PortfolioProject.sql
+++ COVID-PortfolioProject.sql
@@ -1,46 +1,52 @@
-SELECT *
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  WHERE continent is not NULL
-  ORDER BY 3,4
+-- Select all to check if the data was properly imported
+--- The code continent is not NULL is to weed out the grouping location such as "High income" or "North America"
 
--- SELECT *
-  -- FROM [PortfolioProject].[dbo].[CovidVacci]
-  -- ORDER BY 3,4
+SELECT *
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE continent is not NULL
+    ORDER BY 3,4
+
+SELECT *
+    FROM [PortfolioProject].[dbo].[CovidVaccinations]
+    WHERE continent is not NULL
+    ORDER BY 3,4
+
 
 -- Select Data that we are going to be using
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  ORDER BY 1, 2
-
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE continent is not NULL
+    ORDER BY 1, 2
 
 
 -- Looking at the Total Cases vs Total Deaths
--- Shows the likelihood of dying if you contract COVID in your country
+--- Shows the likelihood of dying if you contract COVID in your country
 
 SELECT location, date, total_cases, total_deaths,(CONVERT(float, total_deaths)/ NULLIF(CONVERT(float, total_cases),0))*100 AS DeathPercentage
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  WHERE location LIKE 'Canada'
-  AND continent is not NULL
-  ORDER BY 1, 2
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE location LIKE 'Canada'
+    AND continent is not NULL
+    ORDER BY 1, 2
 
--- About the CONVERT code: I had a lot of troubles importing the .csv file in Azure Data Studio
-    -- the Import Wizard kept populating the data types with erronous type due to a considerable numbers of NULL in the first 1000 rows and also working on a Mac >_<
 
 -- Looking at Total Cases vs Population
 -- Shows the percentage of population got COVID
 
 SELECT location, date, total_cases, Population,(CONVERT(float, total_cases)/ NULLIF(CONVERT(float, Population),0))*100 AS InfectionRate
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  WHERE location LIKE 'Canada'
-  ORDER BY 1, 2
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE location LIKE 'Canada'
+    AND continent is not NULL
+    ORDER BY 1, 2
+
 
 -- Looking at Countries with highest Infection Rate compared to Population
 
 SELECT location, population, MAX(total_cases) AS HighestInfectionCount, (CONVERT (float, MAX(total_cases))/ NULLIF(CONVERT(float, MAX (Population)),0))*100 AS HighestInfectionRate
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  GROUP BY location, Population
-  ORDER BY HighestInfectionRate DESC
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE continent is not NULL
+    GROUP BY location, Population
+    ORDER BY HighestInfectionRate DESC
 
 -- Showing Countries with Highest Death Count per Population
 
@@ -66,17 +72,18 @@ SELECT continent, MAX(total_deaths) AS TotalDeathCount
     GROUP BY continent
     ORDER BY TotalDeathCount DESC
 
+
 -- GLOBAL NUMBERS
--- ******** Ce bout de code ne foncitonne pas correctement, il semble faire un SUM par semaine****
-SELECT SUM(new_cases) AS Total_cases, SUM(new_deaths) AS Total_deaths, 
-    --SUM(new_deaths) / NULLIF(SUM(new_cases) * 1.0, 0) * 100 AS DeathPercentage
-    SUM(new_deaths) / NULLIF(CAST(SUM(new_cases) AS FLOAT), 0) * 100 AS DeathPercentage
-  FROM [PortfolioProject].[dbo].[CovidDeaths]
-  WHERE continent is not NULL
- -- GROUP BY date
-  ORDER BY 1, 2
+SELECT location, SUM(new_cases) AS Total_cases, SUM(new_deaths) AS Total_deaths, 
+SUM(new_deaths) / NULLIF(CAST(SUM(new_cases) AS FLOAT), 0) * 100 AS DeathPercentage
+    FROM [PortfolioProject].[dbo].[CovidDeaths]
+    WHERE continent is not NULL
+    GROUP BY location
+    ORDER BY DeathPercentage DESC
+
 
 -- Looking at Total Population vs Vaccinations
+--- Creating a Rolling count of vaccinated people
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(CONVERT(BIGINT, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, 
@@ -88,6 +95,7 @@ SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
         AND dea.date = vac.date
   WHERE dea.continent is not NULL 
   ORDER BY 2,3
+
 
 -- USE a CTE
 
@@ -105,9 +113,9 @@ AS
     )
 
 SELECT *, (RollingCountPeopleVaccinated/CAST (Population AS FLOAT))*100 AS RollingPeopleVaccinatedPercentage
-FROM PopvsVac
+    FROM PopvsVac
 
--- TEMP TABLE
+-- CREATE A TEMP TABLE
 
 DROP TABLE IF EXISTS #PercentPopulationVaccinated
 CREATE TABLE #PercentPopulationVaccinated
@@ -135,12 +143,19 @@ FROM #PercentPopulationVaccinated
 
 -- CREATE A VIEW TO STORE DATA FOR VISUALIZATION
 
-CREATE VIEW PercentPopulationVaccinated AS
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
-    , SUM(CONVERT(BIGINT, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, 
-    dea.date) AS RollingCountPeopleVaccinated
-    FROM PortfolioProject..CovidDeaths dea
-    JOIN PortfolioProject..CovidVaccinations vac
-        ON dea.location = vac.location
-        AND dea.date = vac.date
-    WHERE dea.continent is not NULL
+CREATE VIEW PercentPopulationVaccinated2 AS
+SELECT 
+    dea.continent, 
+    dea.location, 
+    dea.date, 
+    dea.population, 
+    vac.new_vaccinations,
+    SUM(CONVERT(BIGINT, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingCountPeopleVaccinated
+FROM 
+    PortfolioProject..CovidDeaths dea
+JOIN 
+    PortfolioProject..CovidVaccinations vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
+WHERE 
+    dea.continent is not NULL
